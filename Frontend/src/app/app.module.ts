adcb0e138fb7c1190b51ad722821a3a7705109a4
Andru Shakarov <Anron2012@mail.ru>
2017-11-30 21:06:55 +0300

    Link material design

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material'
+
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+      MatInputModule
   ],
   declarations: [
     AppComponent
